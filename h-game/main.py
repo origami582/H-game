4f80986401d10045e066dbc8c628ed84cbcf08e5
Origami5822 <sodamm2kp@gmail.com>
2025-10-02 22:22:52 +0700

    new

--- h-game/main.py
+++ h-game/main.py
@@ -3,15 +3,37 @@ from py4godot.classes.Control import Control
 
 @gdclass
 class main(Control):
-
+	# popup close
+	def _ready(self):
+		#ready game this pop up close
+		self.get_node("CanvasLayer").visible = False
 	def _on_newgame_pressed(self):
-		self.get_tree().change_scene_to_file("stage\\stage1.tscn")
+		#open popup
+		self.get_node("CanvasLayer").visible = True
 
 	def _on_Continew_pressed(self):
+		#pass
 		print("Continue")
 
 	def _on_lederbord_pressed(self):
+		#pass
 		print("Leaderboard")
-	def _on_exit_pressed(self):
-		 ### exit
+
+	def _on_closepop_pressed(self):
+		#close popup
+		self.get_node("CanvasLayer").visible = False
+
+	#exitgame
+	def _on_exitgame_pressed(self):
 		self.get_tree().quit()
+
+	#difficult select and var to scale with exp_gain and monster status 
+	def _on_easy_pressed(self):
+		difficult = 1
+		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
+	def _on_medium_pressed(self):
+		difficult = 1.5
+		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
+	def _on_hard_pressed(self):
+		difficult = 2
+		self.get_tree().change_scene_to_file("res://stage/stage1.tscn")
