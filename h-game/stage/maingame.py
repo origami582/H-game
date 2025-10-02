b0ad78c2f99237064c50f020b195d63485b039cb
Origami5822 <sodamm2kp@gmail.com>
2025-10-02 22:41:27 +0700

    Globals status

--- h-game/stage/maingame.py
+++ h-game/stage/maingame.py
@@ -1,12 +1,23 @@
 from py4godot.classes import gdclass
 from py4godot.classes.Control import Control
+from .charactor import Globals
 
 @gdclass
 class maingame(Control):
+	def _ready(self):
+		Globals.init_character()
+		Globals.gain_exp(50)
 
+		print(f"Level: {Globals.level}")
+		print(f"EXP: {Globals.exp}/{Globals.exp_req}")
+		print(f"Total EXP: {Globals.exp_total}")
 	def _on_skip_pressed(self):
 		print("skip")
 
 
 	def _on_next_pressed(self):
 		print("next")
+		Globals.gain_exp(50)
+		print(f"Level: {Globals.level}")
+		print(f"EXP: {Globals.exp}/{Globals.exp_req}")
+		print(f"Total EXP: {Globals.exp_total}")
